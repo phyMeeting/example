b4b34189d104f960642d246f3d0e1ee08305e769
adam <adam.chwalczuk@comarch.com>
2017-05-21 23:59:39 +0200

    FIX # in KD 2

--- scr/com/comarch/example/HelloWorld.java
+++ scr/com/comarch/example/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
 
-        System.out.println("we don like KD");
+        System.out.println("we don't like KD");
 
         System.out.println("3th commit");
         System.out.println("Second commit");
