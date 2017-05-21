1f38738d0639e2cc45d249a69e142c9f991e696b
adam <adam.chwalczuk@comarch.com>
2017-05-21 22:47:50 +0200

    FIX # after RC

--- scr/com/comarch/example/HelloWorld.java
+++ scr/com/comarch/example/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
+
+        System.out.println("Not yet on RC");
+
         System.out.println("3th commit");
         System.out.println("Second commit");
         System.out.println("Hello, World");
