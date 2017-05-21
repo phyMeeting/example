20fa3213688cd4a2a7ef101f0a50b4fa8ce496b3
adam <adam.chwalczuk@comarch.com>
2017-05-21 22:40:46 +0200

    FIX # second

--- scr/com/comarch/example/HelloWorld.java
+++ scr/com/comarch/example/HelloWorld.java
@@ -3,6 +3,8 @@ package com.comarch.example;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        System.out.println("Second commit");
         System.out.println("Hello, World");
     }
 
