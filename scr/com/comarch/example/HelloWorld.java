da573abfd339ac65fbe03db7606e6baf406ba760
adam <adam.chwalczuk@comarch.com>
2017-05-21 23:41:40 +0200

    FIX # task 2

--- scr/com/comarch/example/HelloWorld.java
+++ scr/com/comarch/example/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         System.out.println("HEAD is going on");
 
         System.out.println("3th commit - task 1");
-        System.out.println("Second commit");
+        System.out.println("Second commit - tasl 2");
         System.out.println("Hello, World");
     }
 
