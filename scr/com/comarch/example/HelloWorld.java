314f88587e13fbac19cc3b87a40c749785484848
adam <adam.chwalczuk@comarch.com>
2017-05-21 22:44:34 +0200

    FIX # 3th

--- scr/com/comarch/example/HelloWorld.java
+++ scr/com/comarch/example/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
+        System.out.println("3th commit");
         System.out.println("Second commit");
         System.out.println("Hello, World");
     }
