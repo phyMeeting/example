2e8d5ab1c06b6d035f7811e29ca681a5b8bce5e5
adam.chwalczuk@comarch.com <adam.chwalczuk@comarch.com>
2017-05-22 10:49:59 +0200

    task 3

--- scr/com/comarch/example/HelloWorld.java
+++ scr/com/comarch/example/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
 
-        System.out.println("HEAD is going on");
+        System.out.println("HEAD is going on task - 3");
 
         System.out.println("3th commit");
         System.out.println("Second commit");
